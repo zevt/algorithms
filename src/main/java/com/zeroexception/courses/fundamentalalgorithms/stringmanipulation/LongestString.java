75f3206f6fda753db84e9c6cdfd72e832e93d604
Viet <vietsemantics@gmail.com>
2019-08-03 16:23:25 -0500

    longest String

---
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/LongestString.java
@@ -0,0 +1,22 @@
+package com.zeroexception.courses.fundamentalalgorithms.stringmanipulation;
+
+
+import java.util.List;
+
+public interface LongestString {
+
+  /**
+   * Given an array of String,
+   * return the list of longest String from the input array of String
+   * Example:
+   * input = ["no", "yes","x","opp", "now"]
+   * output is a list: ["yes","opp","now"]
+   *
+   * Example:
+   * input = ["know", "bad", "good"]
+   * output = ["know", "good"]
+   */
+  static List<String> longestStrings(String[] inputs) {
+    return null;
+  }
+}
---
+++ src/test/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/LongestStringTest.java
@@ -0,0 +1,10 @@
+package com.zeroexception.courses.fundamentalalgorithms.stringmanipulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Viet Quoc Tran vt on 2019-08-03. www.zeroexception.com
+ */
+class LongestStringTest {
+
+}
