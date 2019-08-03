6e6010e4d86e5c5479102a2df5e0124e6229c9e5
Viet Quoc Tran <viet.tran@siemens.com>
2019-08-03 13:36:51 -0500

    add palindrome

--- src/main/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/StringBasic01.java
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/StringBasic01.java
@@ -1,5 +1,23 @@
 package com.zeroexception.courses.fundamentalalgorithms.stringmanipulation;
 
+/**
+ * Solve basic problems using Java build-in functions manipulating String
+ */
 public class StringBasic01 {
+
+		/**
+		 * Palindrome is a String which give the same result by reading from left to right and from right to left.
+		 * Example: "aabaa" is a palindrome.
+		 * "mom" is a palindrome
+		 * "radar" is a palindrome
+		 * etc.
+		 * Using charAt(int position) function of Java String to check if a input String is palindrome
+		 *
+		 */
+		boolean is(String s) {
+
+			return false;
+		}
+
 
 }
