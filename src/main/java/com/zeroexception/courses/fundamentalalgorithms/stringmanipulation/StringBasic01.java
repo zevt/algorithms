6d1699770c46d38151937e6c3fb2a14315666706
Viet <vietsemantics@gmail.com>
2019-08-10 15:26:48 -0500

    _

--- src/main/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/StringBasic01.java
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/stringmanipulation/StringBasic01.java
@@ -3,7 +3,7 @@ package com.zeroexception.courses.fundamentalalgorithms.stringmanipulation;
 /**
  * Solve basic problems using Java build-in functions manipulating String
  * This exercise aims to put following Java methods into action:
- * charAt(), indexOf(), length(), and substring(), trim(), trim()
+ * charAt(), indexOf(), length(), and substring(), trim()
  * replace()
  * toLowerCase() and toUpperCase()
  * startsWith() and endsWith(),
