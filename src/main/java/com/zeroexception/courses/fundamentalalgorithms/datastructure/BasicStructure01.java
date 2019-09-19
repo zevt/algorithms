df525350d2591b6c473d57a695d6a6eee24762f2
Viet <vietsemantics@gmail.com>
2019-09-19 16:25:43 -0500

    peak and valley

--- src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/BasicStructure01.java
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/BasicStructure01.java
@@ -1,5 +1,26 @@
 package com.zeroexception.courses.fundamentalalgorithms.datastructure;
 
+
 public interface BasicStructure01 {
+
+  /**
+   * The high and the low points of a terrain are represented by an array.
+   * The point which is higher than both nearby points is called PEAK.
+   * The point which lower than both nearby points is called VALLEY.
+   * Given an array represent terrain described above, find all PEAKs and VALLEYs.
+   *
+   * @param terrain
+   * @return array of Peaks in the same order of terrain.
+   */
+  static int[] findPeaks(int[] terrain) {
+
+    return new int[] {};
+  }
+
+  static int[] findValleys(int[] terrain) {
+
+    return new int[] {};
+  }
+
 
 }
