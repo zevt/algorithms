dcfe6b2b6862d8bc95f0b2265468f69b0a9800f2
Viet <vietsemantics@gmail.com>
2019-09-25 19:46:18 -0500

    _

--- src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/list/PracticeList1.java
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/list/PracticeList1.java
@@ -14,9 +14,28 @@ public interface PracticeList1 {
    * Using built-in sort function of java list or collections
    * Using Comparator to pick the field which we want to sort by
    */
-  static public List<Employee> sortEmployeeBySalary(List<Employee> employeeList) {
+  static List<Employee> sortEmployeeBySalary(List<Employee> employeeList) {
     return Collections.emptyList();
   }
+
+
+  /**
+   * Given a list of Employees.
+   * Put them into different group, each group of employees will have the same level of seniority
+   * Use list to store employees who belong to the same group
+   * Inside each group sort employees by their salary.
+   *
+   *
+   * Return a list of these group.
+   * Sort the return list by seniority level. from Junior to Senior
+   * @param employeeList
+   * @return list of group of employee ( in form of list)
+   */
+  static List<List<Employee>> classifySeniority(List<Employee> employeeList) {
+    return Collections.emptyList();
+  }
+
+
 
 
 
---
+++ src/test/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/list/PracticeList1Test.java
@@ -0,0 +1,28 @@
+package com.zeroexception.courses.fundamentalalgorithms.datastructure.list;
+
+import static com.zeroexception.courses.fundamentalalgorithms.datastructure.list.PracticeList1.sortEmployeeBySalary;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Viet Quoc Tran vt on 2019-09-25. www.zeroexception.com
+ */
+class PracticeList1Test {
+
+  @Test
+  void test_sortEmployeeBySalary() {
+    Employee employee = new Employee();
+
+    List<Employee> employees = new ArrayList<>();
+    List<Employee> sortedList = sortEmployeeBySalary(employees);
+
+
+  }
+
+  @Test
+  void test_classifySeniority() {
+
+  }
+}
