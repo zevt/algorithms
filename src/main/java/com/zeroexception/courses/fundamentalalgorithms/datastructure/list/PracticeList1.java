d1167fd586f044f8b54318c839a1845fc7c5ce31
Viet <vietsemantics@gmail.com>
2019-09-25 19:17:59 -0500

    _

--- src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/list/PracticeList1.java
+++ src/main/java/com/zeroexception/courses/fundamentalalgorithms/datastructure/list/PracticeList1.java
@@ -1,6 +1,14 @@
 package com.zeroexception.courses.fundamentalalgorithms.datastructure.list;
 
 
+/**
+ * Using List, ArrayList, LinkedList or array to solve following problem
+ */
 public interface PracticeList1 {
+
+
+
+
+  static public List<>
 
 }
